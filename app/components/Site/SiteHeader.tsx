662fbc44c38f99177e2b9c599764208dde8e0284
Shyam <shyamenk@gmail.com>
2023-06-15 02:46:31 +0530

    ggv

--- app/components/Site/SiteHeader.tsx
+++ app/components/Site/SiteHeader.tsx
@@ -122,7 +122,47 @@ export default function SiteHeader() {
                   )}
                 </div>
               </div>
-              {isAdmin ? null : (
+              {isAdmin ? (
+                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                  {user ? (
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-brand-blue-600 focus:ring-offset-2">
+                          <span className="sr-only">Open user menu</span>
+                          <BiUser className="w-6 h-6 text-brand-gray-500" />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                onClick={logOutHandler}
+                                className={classNames(
+                                  active ? 'bg-brand-gray-100' : '',
+                                  'block px-4  py-2 text-sm text-brand-gray-700 cursor-pointer'
+                                )}
+                              >
+                                Sign out
+                              </a>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  ) : (
+                    <Link href="/login">Login</Link>
+                  )}
+                </div>
+              ) : (
                 <div className="hidden sm:ml-6 sm:flex sm:items-center">
                   <button
                     type="button"
